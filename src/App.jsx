069e99fc37cb2d02a105eeaed5cb87b7b8858b53
Vitalij <vitalij.cernysiov@gmail.com>
2022-09-30 16:52:26 +0300

    Continue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import All from './Components/All';
 import './App.scss';
+import randColor from './Functions/randColor'
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+//   useState [color, setColor ] = ('green')
+
+// useEffect (() => {
+//   setColor(randColor())
+// }, [3000])
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1 style={{
+          color: randColor()
+        }}>Easy meal preparation ideas for creative persons</h1>
        <All />
+       <h1 style={{
+          color: randColor()
+        }}>Delicious food recipes ideas only for you</h1>
       </header>
     </div>
   );
